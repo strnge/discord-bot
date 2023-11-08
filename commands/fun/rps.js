509a43a8687dd83665c58c4899ed828e4b508a66
Max Strange <stra0223@algonquinlive.com>
2023-11-08 14:14:39 -0500

    On branch master  Changes to be committed: 	modified:   commands/fun/rps.js fixed InteractionAlreadyReplied error and correctly ensured buttons cannot be clicked twice

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -36,59 +36,61 @@ module.exports = {
 		const collectorFilter = i => i.user.id === interaction.user.id;
 
 		try {
-			await interaction.editReply({
-				content: 'Choose what to play!',
-				components: [row].setDisabled(true),
-			});
+			// const rowOnClick = new ActionRowBuilder().addComponents(rock.setDisabled(true),
+			// 	paper.setDisabled(true),
+			// 	scissors.setDisabled(true));
+			// response.update({
+			// 	content: 'Choose what to play!',
+			// 	components: [rowOnClick],
+			// });
 			const confirmation = await response.awaitMessageComponent({ filter : collectorFilter, time: 60_000 });
 
 			switch (confirmation.customId) {
 			case 'rock':
 				switch (botRPSChoice) {
 				case 0:
-					confirmation.reply({ content: 'You tied!' });
+					confirmation.update({ content: 'You tied!', components: [] });
 					break;
 				case 1:
-					confirmation.reply({ content: `You lost! You chose ${confirmation.customId} and the bot chose paper!` });
+					confirmation.update({ content: `You lost! You chose ${confirmation.customId} and the bot chose paper!`, components: [] });
 					break;
 				case 2:
-					confirmation.reply({ content: `You win! You chose ${confirmation.customId} and the bot chose scissors!` });
+					confirmation.update({ content: `You win! You chose ${confirmation.customId} and the bot chose scissors!`, components: [] });
 					break;
 				}
 				break;
 			case 'paper':
 				switch (botRPSChoice) {
 				case 0:
-					confirmation.reply({ content: `You win! You chose ${confirmation.customId} and the bot chose rock!` });
+					confirmation.update({ content: `You win! You chose ${confirmation.customId} and the bot chose rock!`, components: [] });
 					break;
 				case 1:
-					confirmation.reply({ content: 'You tied!' });
+					confirmation.update({ content: 'You tied!', components: [] });
 					break;
 				case 2:
-					confirmation.reply({ content: `You lost! You chose ${confirmation.customId} and the bot chose scissors!` });
+					confirmation.update({ content: `You lost! You chose ${confirmation.customId} and the bot chose scissors!`, components: [] });
 					break;
 				}
 				break;
 			case 'scissors':
 				switch (botRPSChoice) {
 				case 0:
-					confirmation.reply({ content: `You lost! You chose ${confirmation.customId} and the bot chose rock!` });
+					confirmation.update({ content: `You lost! You chose ${confirmation.customId} and the bot chose rock!`, components: [] });
 					break;
 				case 1:
-					confirmation.reply({ content: `You win! You chose ${confirmation.customId} and the bot chose paper!` });
+					confirmation.update({ content: `You win! You chose ${confirmation.customId} and the bot chose paper!`, components: [] });
 					break;
 				case 2:
-					confirmation.reply({ content: 'You tied!' });
+					confirmation.update({ content: 'You tied!', components: [] });
 					break;
 				}
 				break;
 			}
-			row.components.setDisabled(true);
+
 		}
 		catch (e) {
 			await interaction.reply({ content: 'Please choose more quickly! (less than 1 minute)' });
 		}
-
 		// TODO: add the game finishing calculations and output
 	},
 };
