62f6baaa6f73bf5b9e72e81d082349c3caa06183
Max Strange <stra0223@algonquinlive.com>
2023-11-08 11:34:36 -0500

    On branch master  Changes to be committed: 	modified:   commands/fun/rps.js 	added base structure for RPS command

--- commands/fun/rps.js
+++ commands/fun/rps.js
@@ -27,11 +27,31 @@ module.exports = {
 		const row = new ActionRowBuilder()
 			.addComponents(rock, paper, scissors);
 
-		await interaction.reply({
+		const response = await interaction.reply({
 			content: 'Choose what to play!',
 			components: [row],
 		});
 
-		
+		const botRPSChoice = Math.floor(Math.random() * 11);
+		const collectorFilter = i => i.user.id ===interaction.user.id;
+
+		try {
+			switch(){
+				case '🪨':
+					//blah
+					break;
+				case '📝':
+					//blah
+					break;
+				case '✂️':
+					//blah
+					break;
+			}
+		}
+		catch(e){
+			
+		}
+
+		// TODO: add the game finishing calculations and output
 	},
 };
